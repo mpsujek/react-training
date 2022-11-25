c72421aa35fe917d1b4c317d3007f8d2b23633a3
mpsujek <mateusz.psujek@gmail.com>
2022-11-25 13:01:40 +0100

    Changes on component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,11 @@
-import React from "react";
-
 export default function ExpenseItem() {
-  return <div>ExpenseItem item</div>;
+  return (
+    <>
+      <div>March 28th 2022</div>
+      <div>
+        <h2>Car Insurance</h2>
+        <div>$294,67</div>
+      </div>
+    </>
+  );
 }
