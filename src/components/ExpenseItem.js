63340c3735bdc721b8ea41cc49f1c97237cb05e6
mpsujek <mateusz.psujek@gmail.com>
2022-11-25 14:51:21 +0100

    Styling of component added

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,11 +1,13 @@
+import "./ExpenseItem.css";
+
 export default function ExpenseItem() {
   return (
-    <>
+    <div className="expense-item">
       <div>March 28th 2022</div>
-      <div>
+      <div className="expense-item__description">
         <h2>Car Insurance</h2>
-        <div>$294,67</div>
+        <div className="expense-item__price">$294,67</div>
       </div>
-    </>
+    </div>
   );
 }
