40b7f0050b29a5e0daf92d71900df02e3ea95ad1
mpsujek <mateusz.psujek@gmail.com>
2022-11-25 15:11:32 +0100

    store data in variables

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,15 @@
 import "./ExpenseItem.css";
 
 export default function ExpenseItem() {
+  const expenseDate = new Date(2001, 2, 28);
+  const expenseTitle = "Car Insurance";
+  const expenseAmount = 294.67;
   return (
     <div className="expense-item">
-      <div>March 28th 2022</div>
+      <div>{expenseDate.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>Car Insurance</h2>
-        <div className="expense-item__price">$294,67</div>
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">${expenseAmount}</div>
       </div>
     </div>
   );
