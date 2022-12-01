2869749c9b0e47d49e863a22645cf711bc364106
mpsujek <mateusz.psujek@gmail.com>
2022-12-01 15:37:34 +0100

    Change to add more redability in code

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -12,24 +12,27 @@ export default function Expenses({ expenses }) {
   const filteredExpenses = expenses.filter((expense) => {
     return expense.date.getFullYear().toString() === yearFilter;
   });
+
+  let expenseContent = <p>No expenses found.</p>;
+
+  if (filteredExpenses.length > 0) {
+    expenseContent = filteredExpenses.map((expense) => (
+      <ExpenseItem
+        key={expense.id}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+      />
+    ));
+  }
+
   return (
     <Card className='expenses'>
       <ExpensesFilter
         yearFilter={yearFilter}
         onFilterChange={yearFilterChangeHanlder}
       />
-      {filteredExpenses.length === 0 ? (
-        <p>No expenses found.</p>
-      ) : (
-        filteredExpenses.map((expense) => (
-          <ExpenseItem
-            key={expense.id}
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-          />
-        ))
-      )}
+      {expenseContent}
     </Card>
   );
 }
